6dde85933288fc1934b016416f0eaa9d08d18d58
KIM DONG IL <cappu159@gmail.com>
2021-07-17 16:59:39 +0900

    create firestore

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,8 @@
 import firebase from "firebase/app";
 import "firebase/auth";
+import "firebase/firestore";
+
+import { cityDb } from "./temp/m-city-export";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCCOMEh_9m7cY1pkD8nOy7nkV1WjUSC3PA",
@@ -15,7 +18,41 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 // firebase.analytics();
 
-export { firebase };
+const DB = firebase.firestore();
+const matchesCollection = DB.collection("matches");
+const playersCollection = DB.collection("players");
+const positionsCollection = DB.collection("positions");
+const promotionsCollection = DB.collection("promotions");
+const teamsCollection = DB.collection("teams");
+
+// cityDb.matches.forEach((item) => {
+//   matchesCollection.add(item);
+// });
+
+// cityDb.players.forEach((item) => {
+//   playersCollection.add(item);
+// });
+
+// cityDb.positions.forEach((item) => {
+//   positionsCollection.add(item);
+// });
+
+// cityDb.promotions.forEach((item) => {
+//   promotionsCollection.add(item);
+// });
+
+cityDb.teams.forEach((item) => {
+  teamsCollection.add(item);
+});
+
+export {
+  firebase,
+  matchesCollection,
+  playersCollection,
+  positionsCollection,
+  promotionsCollection,
+  teamsCollection,
+};
 
 // admin@demo.com
 // admin123
